c6085a1052f975a88f496d8a0002de86d12ab0b2
Belson <belson@server.local>
2014-10-27 12:23:37 +0800

    include options as parameter

--- lib/spidercrawl.rb
+++ lib/spidercrawl.rb
@@ -1,8 +1,8 @@
 require "spidercrawl/spider_worker"
 
 class Spiderman
-  def self.shoot(urls, &block)
-	spiderman = Spidercrawl::SpiderWorker.new(urls)
+  def self.shoot(urls, options, &block)
+	spiderman = Spidercrawl::SpiderWorker.new(urls, options)
 	block.call(spiderman)
 	spiderman.crawl
   end
