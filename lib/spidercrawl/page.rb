423a1d358fb7754614217aec88e1e5cc06385037
Belson <belson@server.local>
2014-10-27 16:19:42 +0800

    todo: meta keywords and descriptions

--- lib/spidercrawl/page.rb
+++ lib/spidercrawl/page.rb
@@ -6,83 +6,87 @@ module Spidercrawl
 
     attr_reader :location
 
-	def initialize(url, options = {})
-	  @url = url
-	  @code = options[:response_code]
-	  @headers = options[:response_head]
-	  @location = options[:redirect_url]
-	  @body = options[:response_body]
-	  @time = options[:response_time]
-	end
+    def initialize(url, options = {})
+      @url = url
+      @code = options[:response_code]
+      @headers = options[:response_head]
+      @location = options[:redirect_url]
+      @body = options[:response_body]
+      @time = options[:response_time]
+    end
 
     #
     # Return the url of the page
     #
-	def url
-	  @url.to_s
-	end
+    def url
+      @url.to_s
+    end
 
     #
     # Return the url scheme of the page (e.g. http, https, etc.)
     #
-	def scheme
-	  @url.scheme
-	end
+    def scheme
+      @url.scheme
+    end
 
     #
     # Return the url host of the page
     #
-	def host
-	  @url.host
-	end
+    def host
+      @url.host
+    end
 
     #
     # Return the base url of the page
     #
-	def base_url
-	  @base_url = "#{scheme}://#{host}"
-	end
+    def base_url
+      @base_url = "#{scheme}://#{host}"
+    end
 
-	#
-	# Return the Nokogiri html document
-	#
-	def doc
-	  @document = Nokogiri::HTML(@body)
-	rescue Exception => e
-	  puts e.inspect
-	  puts e.backtrace
-	end
+    #
+    # Return the Nokogiri html document
+    #
+    def doc
+      @document = Nokogiri::HTML(@body)
+      rescue Exception => e
+        puts e.inspect
+        puts e.backtrace
+    end
 
+    #
+    # Return the headers of the page
+    #
     def headers
       puts @headers
     end
-	#
-	# Return the title of the page
-	#
-	def title
-	  @title = doc.css('head title').inner_text
-	end
-
-	#
-	# Return the entire links found in the page; exclude empty links
-	#
-	def links
-	  @links = doc.css('a').map { |link| link['href'].to_s }.uniq.delete_if { |href| href.empty? }.map { |url| absolutify(url) }
+    
+    #
+    # Return the title of the page
+    #
+    def title
+      @title = doc.css('head title').inner_text
     end
 
-	#
-	# Return the internal links found in the page
-	#
-	def internal_links
-	  @internal_links = links.select { |link| URI(link).host == host }
-	end
+    #
+    # Return the entire links found in the page; exclude empty links
+    #
+    def links
+      @links = doc.css('a').map { |link| link['href'].to_s }.uniq.delete_if { |href| href.empty? }.map { |url| absolutify(url) }
+    end
 
-	#
-	# Return the external links found in the page
-	#
-	def external_links
-	  @external_links = links.select { |link| URI(link).host != host }
-	end
+    #
+    # Return the internal links found in the page
+    #
+    def internal_links
+      @internal_links = links.select { |link| URI(link).host == host }
+    end
+
+    #
+    # Return the external links found in the page
+    #
+    def external_links
+      @external_links = links.select { |link| URI(link).host != host }
+    end
 
     #
     # Return any emails found in the page
@@ -94,9 +98,9 @@ module Spidercrawl
     #
     # Return all images found in the page
     #
-	def images
+    def images
       @images = doc.css('img').map { |img| img['src'].to_s }.uniq.delete_if { |src| src.empty? }.map { |url| absolutify(url) }
-	end
+    end
 
     #
     # Return all words found in the page
@@ -128,8 +132,8 @@ module Spidercrawl
     #
     # Return the content type of the page
     #
-	def content_type
-	  doc.at("meta[@http-equiv='Content-Type']")['content']
+    def content_type
+      doc.at("meta[@http-equiv='Content-Type']")['content']
     end
 
     # 
@@ -158,9 +162,9 @@ module Spidercrawl
     #
     # Return true if page not found 
     #
-	def not_found?
-	  @code == 404
-	end
+    def not_found?
+      @code == 404
+    end
 
     #
     # Return true if page is fetched successfully
@@ -176,14 +180,14 @@ module Spidercrawl
       (300..307).include?(@code)
     end
 
-	#
-	# Return the absolute url without query params, etc.
-	#
-	private
-	def absolutify(page_url)
+    #
+    # Return the absolute url without query params, etc.
+    #
+    private
+    def absolutify(page_url)
       page_url = page_url.split('?')[0].split('#')[0]
-	  return page_url if URI(page_url).absolute?
-	  URI.join(base_url, page_url).to_s
-	end
+      return page_url if URI(page_url).absolute?
+      URI.join(base_url, page_url).to_s
+    end
   end
 end
