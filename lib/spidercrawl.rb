eda50230f99981c045af4a34b92d2790b73a2234
Belson <belson@server.local>
2014-10-27 15:31:02 +0800

    change proc call to yield

--- lib/spidercrawl.rb
+++ lib/spidercrawl.rb
@@ -2,8 +2,8 @@ require "spidercrawl/spider_worker"
 
 class Spiderman
   def self.shoot(urls, options, &block)
-	spiderman = Spidercrawl::SpiderWorker.new(urls, options)
-	yield spiderman if block_given?
-	spiderman.crawl
+    spiderman = Spidercrawl::SpiderWorker.new(urls, options)
+    yield spiderman if block_given?
+    spiderman.crawl
   end
 end
