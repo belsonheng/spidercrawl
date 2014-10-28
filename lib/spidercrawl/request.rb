4b4c30d90102ec4a76337d7f161f50c84e4a746a
Belson <belson@server.local>
2014-10-28 16:30:48 +0800

    use colorize

--- lib/spidercrawl/request.rb
+++ lib/spidercrawl/request.rb
@@ -1,50 +1,53 @@
 require 'spidercrawl/page'
 require 'spidercrawl/user_agents'
 require 'net/http'
-require 'typhoeus'
+require 'colorize'
+#require 'typhoeus'
 
 module Spidercrawl
   # Makes the request to the targeted website
   class Request
 
-    attr_accessor :url
+    attr_accessor :uri
 
     def initialize(url, options = {})
-      @url = url
+      @uri = URI.parse(url)
       @threads = options[:threads]
       @timeout = options[:timeout]
+
+      @http = Net::HTTP.new(@uri.host, @uri.port) do |http|
+        http.open_timeout = @timeout # in seconds
+        http.read_timeout = @timeout # in seconds
+      end
     end
 
     #
     # Fetch a page from the given url
     #
     def fetch
-      puts "fetching #{@url}"
-      uri = URI(@url)
+      puts "fetching #{@uri.to_s}".green.on_black
       start_time = Time.now
       begin
-        Net::HTTP.start(uri.host, uri.port) do |http|
-          http.open_timeout = @timeout # in seconds
-          http.read_timeout = @timeout # in seconds
-          response = http.get(uri)
-          end_time = Time.now
-          case response
-          when Net::HTTPSuccess then
-            page = Page.new(uri, response_code: response.code.to_i,
-                                 response_head: response.instance_variable_get("@header"),
-                                 response_body: response.body,
-                                 response_time: ((end_time-start_time)*1000).round,
-                                 crawled_time: (Time.now.to_f*1000).to_i)
-          when Net::HTTPRedirection then
-            page = Page.new(uri, response_code: response.code.to_i,
-                                 response_head: response.instance_variable_get("@header"),
-                                 response_body: response.body,
-                                 response_time: ((end_time-start_time)*1000).round,
-                                 redirect_url:  response['location'])
-          when Net::HTTPNotFound then
-            page = Page.new(uri, response_code: response.code.to_i,
-                                 response_time: ((end_time-start_time)*1000).round)
-          end
+        request = Net::HTTP::Get.new(@uri.request_uri)
+        request["User-Agent"] = UserAgents.random
+        response = @http.request(request) 
+        end_time = Time.now
+        case response
+        when Net::HTTPSuccess then
+          page = Page.new(@uri, response_code: response.code.to_i,
+                                response_head: response.instance_variable_get("@header"),
+                                response_body: response.body,
+                                response_time: ((end_time-start_time)*1000).round,
+                                crawled_time: (Time.now.to_f*1000).to_i)
+        when Net::HTTPRedirection then
+          page = Page.new(@uri, response_code: response.code.to_i,
+                                response_head: response.instance_variable_get("@header"),
+                                response_body: response.body,
+                                response_time: ((end_time-start_time)*1000).round,
+                                redirect_url:  response['location'])
+        when Net::HTTPNotFound then
+          page = Page.new(@uri, response_code: response.code.to_i,
+                                response_time: ((end_time-start_time)*1000).round)
         end
       rescue Exception => e
         puts e.inspect
@@ -53,8 +56,11 @@ module Spidercrawl
     end
   end
   # Makes parallel requests to the targeted website using typhoeus and hydra
+=begin
   class ParallelRequest
     
+    attr_accessor :urls
+
     def initialize(urls, options = {})
       @urls = urls
       @threads = options[:threads]
@@ -66,18 +72,36 @@ module Spidercrawl
     #
     def fetch
       hydra = Typhoeus::Hydra.new(:max_concurrency => @threads)
+      page, pages = nil, []
 
       @urls.each do |url|
-        puts "fetching #{url}"
-        request = Typhoeus::Request.new(url, :timeout => @timeout, :follow_location => false, :headers => {"User-Agent" => UserAgents.random})
-        request.on_headers do |response|
-          puts "Success: #{url}" if response.success?
-          puts "Redirect: #{response.headers['Location']}" if (300..307).include?(response.code)
+        request = Typhoeus::Request.new(url, :timeout => @timeout, :followlocation => false, :headers => {"User-Agent" => UserAgents.random})
+        request.on_complete do |response|
+          uri = URI(url)
+          if response.success?
+          	puts "fetching #{url}"
+            page = Page.new(uri, response_code: response.code,
+                                 response_head: response.headers,
+                                 response_body: response.body,
+                                 response_time: response.total_time,
+                                 crawled_time: (Time.now.to_f*1000).to_i)
+          elsif (300..307).include?(response.code)
+            page = Page.new(uri, response_code: response.code,
+                                 response_head: response.headers,
+                                 response_body: response.body,
+                                 response_time: response.total_time,
+                                 redirect_url:  response.headers['Location'])
+          elsif 404 == response.code
+            page = Page.new(uri, response_code: response.code,
+                                 response_time: response.total_time)
+          end
+          pages << page
         end
         hydra.queue(request)
       end
 
       hydra.run
+      return pages
     end
-  end
+=end
 end
