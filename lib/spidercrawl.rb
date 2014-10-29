b1647917eefd0648fb8144cac00ce902d0b36c5f
Belson <belson@server.local>
2014-10-29 13:34:29 +0800

    add options for parallel crawl

--- lib/spidercrawl.rb
+++ lib/spidercrawl.rb
@@ -4,6 +4,7 @@ class Spiderman
   def self.shoot(urls, options, &block)
     spiderman = Spidercrawl::SpiderWorker.new(urls, options)
     yield spiderman if block_given?
-    spiderman.crawl
+    spiderman.parallel_crawl if options[:parallel] == true
+    spiderman.crawl if options[:parallel] == false
   end
 end
